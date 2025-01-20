02a1fb3e437d281d7218808c7f044407653d4b0d
thiendoan1208 <thienmacro1234@gmail.com>
2025-01-19 19:18:18 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="width-full h-screen flex justify-center items-center">
+      <div className="w-6/12 flex item-center">
+      <div className="w-1/2 flex  justify-center"> 
+        <div className="w-1/2 h-full border-sky-300  overflow-hidden">
+          <img className="width-full h-full object-cover object-center" src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="" />
+        </div>
+      </div>
+
+      <div>
+        <h1>David Grant</h1>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
