02bf2b99abe5b3ac6042170b5171fe6f2bb752bb
thiendoan1208 <thienmacro1234@gmail.com>
2025-01-21 19:28:09 -0500

    Header #1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
+import Header from "./Components/Header";
+import TableUsers from "./Components/TableUsers";
+
 function App() {
   return (
-    <div className="width-full h-screen flex justify-center items-center">
-      <div className="w-6/12 flex item-center">
-      <div className="w-1/2 flex  justify-center"> 
-        <div className="w-1/2 h-full border-sky-300  overflow-hidden">
-          <img className="width-full h-full object-cover object-center" src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="" />
-        </div>
-      </div>
-
-      <div>
-        <h1>David Grant</h1>
-      </div>
-      </div>
+    <div className="app__container">
+      <Header/>
+      <TableUsers/>
     </div>
   );
 }
---
+++ src/Components/TableUsers/index.js
@@ -0,0 +1,10 @@
+import styles from './TableUsers.module.scss';
+
+function TableUsers(props) {
+    return ( 
+    <div>
+     TableUsers
+    </div> );
+}
+
+export default TableUsers;
